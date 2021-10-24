ebd362ca2f277c07121dfba9aa054ffaa156b742
Faraz Nizamani <53582614+farazahmeds@users.noreply.github.com>
2021-10-24 19:08:23 +0200

    Create 3dconv.py

---
+++ stems/3dconv.py
@@ -0,0 +1,16 @@
+import torch.nn as nn
+from torch.nn import Sequential
+
+
+class modifybasicstem(nn.Sequential):
+    """The default conv-batchnorm-relu stem
+    """
+
+    def __init__(self):
+        super(modifybasicstem, self).__init__(
+            nn.Conv3d(1, 64, kernel_size=(3, 7, 7), stride=(1, 2, 2),
+                      padding=(1, 3, 3), bias=False),
+            nn.BatchNorm3d(64),
+            nn.ReLU(inplace=True))
+
+    print ('this is 3d cov')
