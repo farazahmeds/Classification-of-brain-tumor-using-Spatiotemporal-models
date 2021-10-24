c9812ce9f772ee73fe61da522b85d3e7b0e1e3f2
Faraz Nizamani <53582614+farazahmeds@users.noreply.github.com>
2021-10-24 19:08:43 +0200

    Create resnet2p1.py

---
+++ stems/resnet2p1.py
@@ -0,0 +1,24 @@
+#R(2+1)D Stem to override
+
+import torch.nn as nn
+from torch.nn import Sequential
+
+class R2Plus1dStem4MRI(nn.Sequential):
+    """R(2+1)D stem is different than the default one as it uses separated 3D convolution
+    """
+
+    def __init__(self):
+        super(R2Plus1dStem4MRI, self).__init__(
+            nn.Conv3d(1, 45, kernel_size=(1, 7, 7),
+                      stride=(1, 2, 2), padding=(0, 3, 3),
+                      bias=False),
+            nn.BatchNorm3d(45),
+            nn.ReLU(inplace=True),
+
+            nn.Conv3d(45, 64, kernel_size=(3, 1, 1),
+                      stride=(1, 1, 1), padding=(1, 0, 0),
+                      bias=False),
+            nn.BatchNorm3d(64),
+            nn.ReLU(inplace=True))
+
+        print ('this is two plus1')
